29d959f6b46f4bd7ddfb96385c8858c1b21c7437
logicp <adventurist@gmail.com>
2020-03-07 16:14:08 -0500

    attempting to troubleshoot filedialog text appearance

--- src/argdialog.cpp
+++ src/argdialog.cpp
@@ -19,7 +19,7 @@ ArgDialog::ArgDialog(QWidget *parent) :
     ui->setupUi(this);
     QObject::connect(ui->addFile, &QPushButton::clicked, this, [this]() {
         QFileDialog file_dialog = QFileDialog(this);
-        file_dialog.setStyleSheet("QWidget { color: white; font-weight: 700}");
+        file_dialog.setStyleSheet("QFileDialog QWidget { color: white; font-weight: 700}");
         auto file_path = file_dialog.getOpenFileName(this,
                                                 tr("Open File"), "~", tr("All Files (*.*)"));
         qDebug() << "Selected file:" << file_path;
