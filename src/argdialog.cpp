8833515779e00ba6e7f6174dca79fb2979d8be25
logicp <adventurist@gmail.com>
2020-03-08 12:11:23 -0400

    setting arg type to first index upon clear/default

--- src/argdialog.cpp
+++ src/argdialog.cpp
@@ -198,6 +198,7 @@ void ArgDialog::clearPost() {
     m_ig_post.requested_by.clear();
     m_ig_post.promote_share = "Share the post through IG story if you enjoy the phrase 🙋‍♀️";
     m_ig_post.requested_by_phrase = "The phrase was requested by ";
+    ui->argType->setCurrentIndex(0);
 }
 
 void ArgDialog::clearTask() {
