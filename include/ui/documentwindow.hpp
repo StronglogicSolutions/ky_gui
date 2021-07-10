451a31d268a3154f20e18689c4c3716d9d8af7ba
logicp <logicp@logicp.ca>
2021-07-10 01:53:42 -0400

    able to click-add tokens to table widget

--- include/ui/documentwindow.hpp
+++ include/ui/documentwindow.hpp
@@ -2,6 +2,7 @@
 #define DOCUMENTWINDOW_H
 
 #include <QMainWindow>
+#include <QMouseEvent>
 
 namespace Ui {
 class DocumentWindow;
@@ -17,9 +18,14 @@ public:
 
   void set_flags(const QVector<QString>& flags);
 
+protected:
+  void mouseReleaseEvent(QMouseEvent* e);
+
 private:
+  void set_inserting(const int32_t& index);
   Ui::DocumentWindow *ui;
   QVector<QString>   m_flags;
+  int32_t            m_inserting;
 };
 
 #endif // DOCUMENTWINDOW_H
--- src/window/document/documentwindow.cpp
+++ src/window/document/documentwindow.cpp
@@ -1,12 +1,31 @@
 #include "include/ui/documentwindow.hpp"
 #include "ui_documentwindow.h"
 #include <QDebug>
+#include <QList>
+#include <QTableWidget>
 
 DocumentWindow::DocumentWindow(QWidget *parent) :
   QMainWindow(parent),
-  ui(new Ui::DocumentWindow)
+  ui(new Ui::DocumentWindow),
+  m_inserting(-1)
 {
   ui->setupUi(this);
+
+  QObject::connect(ui->tokens, static_cast<void (QComboBox::*)(int)>(&QComboBox::currentIndexChanged), this,
+    [this](int index)
+    {
+      set_inserting(index);
+    }
+  );
+
+  QObject::connect(ui->rowContent, &QTableWidget::clicked, this, [this](const QModelIndex& index)
+  {
+    auto col = index.column();
+    auto row = index.row();
+    auto widget = new QTableWidgetItem{};
+    widget->setText(m_flags.at(m_inserting));
+    ui->rowContent->setItem(row, col, widget);
+  });
 }
 
 DocumentWindow::~DocumentWindow()
@@ -18,4 +37,26 @@ void DocumentWindow::set_flags(const QVector<QString>& flags)
 {
   qDebug() << "Setting flags in Document Window";
   m_flags = flags;
+  ui->tokens->addItems(flags.toList());
+  setCursor(Qt::CursorShape::ArrowCursor);
+}
+
+void DocumentWindow::set_inserting(const int32_t& index)
+{
+  m_inserting = index;
+  setCursor(Qt::CursorShape::CrossCursor);
+}
+
+
+void DocumentWindow::mouseReleaseEvent(QMouseEvent* e)
+{
+  if (e->button() == Qt::LeftButton)
+  {
+    qDebug() << "Mouse pressed";
+    if (m_inserting > -1)
+    {
+      set_inserting(-1);
+      setCursor(Qt::CursorShape::ArrowCursor);
+    }
+  }
 }
